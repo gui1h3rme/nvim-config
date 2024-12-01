3e23c04d88f074e7f6e650715790e6ac9c5200f4
Guilherme Faria <guilherme.crf@outlook.com.br>
2024-12-01 13:20:00 -0300

    implement lsp-status and nvim-navic into lualine

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -2,13 +2,40 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'gruvbox',
+    disabled_filetypes = {
+      winbar = { 'packer', 'NvimTree' }
+    },
+    globalstatus = true
   },
   sections = {
+    lualine_a = { 'branch' },
+    lualine_b = {
+      { 'filename', path = 1 }
+    },
+    lualine_c = { 'diff' },
+    lualine_x = { 'location' },
+    lualine_y = { 'encoding', 'fileformat', { 'filetype', icon = nil } },
+    lualine_z = { "require'lsp-status'.status()" },
+  },
+  winbar = {
+    lualine_c = { },
     lualine_a = {
-      {
-        'filename',
-        path = 1,
-      }
+      { 'filetype', icon_only = true, colored = false, separator = '' },
+    },
+    lualine_b = {
+      { 'filename', path = 0, file_status = false },
+    },
+    lualine_x = {
+      { 'navic' }
+    }
+  },
+  inactive_winbar = {
+    lualine_a = {},
+    lualibe_b = {},
+    lualine_c = {
+      { 'filetype', icon_only = true, colored = false, separator = '' },
+      { 'filename', path = 0, file_status = false },
     }
   }
 }
+
