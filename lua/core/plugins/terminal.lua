361a823b0cd21bc9bbd21b0a8f69f515d00fedc1
Guilherme Faria <guilherme.crf@outlook.com.br>
2024-12-01 13:17:56 -0300

    add terminal settings for powershell

--- lua/core/plugins/terminal.lua
+++ lua/core/plugins/terminal.lua
@@ -1,3 +1,12 @@
 require('toggleterm').setup {
-  open_mapping = [[<Super-\>]]
+  size = 10,
+  open_mapping = [[<C-/>]],
+  start_in_insert = true,
+  direction = "float",
+  shell = "powershell.exe",
+  float_opts = {
+    border = "curved",
+    width = math.ceil(vim.o.columns*0.8),
+    height = math.ceil(vim.o.columns*0.2)
+  }
 }
