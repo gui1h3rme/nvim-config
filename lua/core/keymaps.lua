9d9620e08e02ecc42d3df7c31414f7f86a4b9cb7
Guilherme Faria <guilherme.crf@outlook.com.br>
2024-12-01 13:20:34 -0300

    add comments to keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,26 +15,31 @@ vim.opt.expandtab = true
 
 vim.wo.number = true
 
+-- Switch buffers
 vim.keymap.set('n', '<Space><Left>', '<C-w>h')
 vim.keymap.set('n', '<Space><Down>', '<C-w>j')
 vim.keymap.set('n', '<Space><Up>', '<C-w>k')
 vim.keymap.set('n', '<Space><Right>', '<C-w>l')
 
+-- Move current buffer
 vim.keymap.set('n', '<Space><C-Left>', '<C-w>H')
 vim.keymap.set('n', '<Space><C-Down>', '<C-w>J')
 vim.keymap.set('n', '<Space><C-Up>', '<C-w>K')
 vim.keymap.set('n', '<Space><C-Right>', '<C-w>L')
 
+-- Resize current buffer
 vim.keymap.set('n', '<S-Up>', '<C-w>-')
 vim.keymap.set('n', '<S-Down>', '<C-w>+')
 vim.keymap.set('n', '<S-Left>', '<C-w>>')
 vim.keymap.set('n', '<S-Right>', '<C-w><')
 
+-- Split current buffer 
 vim.keymap.set('n', '<C-Space-Up>', '<C-w>s')
 vim.keymap.set('n', '<C-Space-Down>', '<C-w>s')
 vim.keymap.set('n', '<C-Space-Left>', '<C-w>v')
 vim.keymap.set('n', '<C-Space-Right>', '<C-w>v')
 
+-- Quit current buffer
 vim.keymap.set('n', '<Space><ESC>', '<C-w>q')
 
 vim.keymap.set('v', '<Space>r', 'y:%s/<C-r>0//g<Left><Left>', { desc = 'Search/replace visual' })
