0fa9262584ad761c067180898efd81c9af093455
Guilherme Faria <guilherme.crf@outlook.com.br>
2024-12-01 21:55:39 -0300

    add terminal setup to allow linux and windows setup

--- lua/core/plugins/terminal.lua
+++ lua/core/plugins/terminal.lua
@@ -1,12 +1,17 @@
-require('toggleterm').setup {
+local options = {
   size = 10,
   open_mapping = [[<C-/>]],
   start_in_insert = true,
-  direction = "float",
-  shell = "powershell.exe",
+  direction = 'float',
   float_opts = {
-    border = "curved",
+    border = 'curved',
     width = math.ceil(vim.o.columns*0.8),
     height = math.ceil(vim.o.columns*0.2)
   }
 }
+
+if vim.fn.has('macunix') == false then
+  options.shell = 'powershell.exe'
+end
+
+require('toggleterm').setup(options)
