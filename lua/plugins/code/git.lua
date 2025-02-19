9db8b2f621c2cadb9bec491683441c87145dec01
Guilherme Faria <guilherme.crf@outlook.com.br>
2025-02-19 09:24:56 -0300

    add setup for gitsigns

--- lua/plugins/code/git.lua
+++ lua/plugins/code/git.lua
@@ -1,5 +1,17 @@
 return {
   'f-person/git-blame.nvim',
-  'lewis6991/gitsigns.nvim',
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "-" },
+        changedelete = { text = "x" },
+        untracked = { text = "!" },
+      },
+    },
+    event = 'VimEnter'
+  },
   'tpope/vim-fugitive',
 }
