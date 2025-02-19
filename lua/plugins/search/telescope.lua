1045a56f3fcbdbfd1a451009a3ea7cbc6e4bf953
Guilherme Faria <guilherme.crf@outlook.com.br>
2025-02-19 10:50:16 -0300

    update plugins

--- lua/plugins/search/telescope.lua
+++ lua/plugins/search/telescope.lua
@@ -1,16 +1,20 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.x',
   event = "VimEnter",
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-live-grep-args.nvim',
-    'ahmedkhalf/project.nvim',
+    {
+      'ahmedkhalf/project.nvim',
+      config = function ()
+        require('project_nvim').setup({})
+      end
+    }
   },
   keys = {
-    { '<C-p>'     , require('telescope.builtin').find_files,                       desc = "Search files" },
-    { '<Leader>fr', require('telescope.builtin').resume,                           desc = "Resume Telescope" },
-    { '<C-[>'     , '<CMD>Telescope projects<CR>',                                         desc = "Search projects" }
+    { '<C-p>'     , require('telescope.builtin').find_files, desc = "Search files" },
+    { '<Leader>fr', require('telescope.builtin').resume,     desc = "Search resume" },
+    { '<C-[>'     , '<CMD>Telescope projects<CR>',           desc = "Search projects" }
   },
   config = function()
     require('telescope').load_extension('live_grep_args')
